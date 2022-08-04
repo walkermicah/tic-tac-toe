d524f7e7f44140453aebaa8f2f535f876762c33e
Walker <walker.m.oliver@gmail.com>
2022-08-04 08:56:52 -0400

    Add logic allowing players to take turns

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 "use strict";
 
 const GameBoard = (() => {
-  const boardArray = [
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-  ];
+  const boardArray = new Array(9).fill(undefined);
 
   const updateGameboard = () => {
     const cells = document.querySelectorAll(".cell");
@@ -28,7 +18,8 @@ const GameBoard = (() => {
     updateGameboard();
   };
 
-  const checkForWinner = (mark) => {
+  const checkForWinner = (active) => {
+    let winner;
     const winningCombinations = [
       [0, 1, 2],
       [3, 4, 5],
@@ -41,29 +32,71 @@ const GameBoard = (() => {
     ];
 
     winningCombinations.forEach((arr) => {
-      if (arr.every((cell) => boardArray[cell] === mark)) console.log("WIN");
+      if (arr.every((cell) => boardArray[cell] === active.mark)) {
+        console.log("WIN");
+        winner = active.player;
+        GameController.resetGame();
+      }
     });
+
+    if (!winner) GameController.switchActivePlayer();
+    //if winner, instead of cl: get winner
+    //call congratulateWinner function on player that won
+    //call newGameButton function from gameController
   };
 
   const checkForTie = () => {
-    if (boardArray.every(Boolean)) console.log("TIE");
-    //instead of cl, display winner & call newGameButton function
+    if (boardArray.every(Boolean)) {
+      console.log("TIE");
+      GameController.resetGame();
+    }
   };
 
-  return { addMarkToBoardArray, checkForWinner, checkForTie };
+  const resetBoard = () => {
+    boardArray.fill(undefined);
+  };
+
+  return { addMarkToBoardArray, checkForWinner, checkForTie, resetBoard };
 })();
 
 const GameController = (() => {
-  const mark = "X";
-
   const gameBoard = document.querySelector(".gameboard");
+
+  const p1 = {
+    mark: "X",
+    player: "player1",
+  };
+  const p2 = {
+    mark: "O",
+    player: "player2",
+  };
+  let active = p1;
+
+  const switchActivePlayer = () =>
+    active === p1 ? (active = p2) : (active = p1);
+
+  const resetGame = () => {
+    GameBoard.resetBoard();
+    active = p1;
+  };
+
+  //new game button function: button appears and calls resetGame when pressed
 
   gameBoard.addEventListener("click", function (e) {
     if (!e.target.classList.contains("cell")) return;
-    GameBoard.addMarkToBoardArray(e.target.dataset.number, mark);
-    GameBoard.checkForWinner(mark);
+    GameBoard.addMarkToBoardArray(e.target.dataset.number, active.mark);
+    GameBoard.checkForWinner(active);
     GameBoard.checkForTie();
   });
+
+  return { switchActivePlayer, resetGame };
 })();
 
-const Player = (name) => {};
+const Player = (name) => {
+  //-event listener to get name typed in. when name is submitted, display it in div
+  //function to highlight player when it's their turn
+  //function to congratulate winner
+};
+
+const player1 = Player("Player 1");
+const player2 = Player("Player 2");
