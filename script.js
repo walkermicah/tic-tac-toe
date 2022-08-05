feb16341c478b737b17e85450d628ad1e1db189d
Walker <walker.m.oliver@gmail.com>
2022-08-05 11:56:24 -0400

    Allow players to change names

--- script.js
+++ script.js
@@ -36,11 +36,7 @@ const Player = (name, mark) => {
     name = newName;
   };
 
-  const congratulateWinner = () => {
-    console.log(`Winner: ${name}`);
-  };
-
-  return { getName, getMark, changeName, congratulateWinner };
+  return { getName, getMark, changeName };
 };
 
 const player1 = Player("Player 1", "X");
@@ -49,22 +45,33 @@ const player2 = Player("Player 2", "O");
 const GameController = (() => {
   const gameBoard = document.querySelector(".gameboard");
   const newGameBtn = document.querySelector(".new-game-btn");
+  const playerDisplay = document.querySelector(".player-container");
+  const player1Display = document.getElementById("player-1");
+  const player1Name = document.getElementById("name-1");
+  const player1Form = document.getElementById("form-1");
+  const player1Input = document.getElementById("form-input-1");
+  const player2Display = document.getElementById("player-2");
+  const player2Name = document.getElementById("name-2");
+  const player2Form = document.getElementById("form-2");
+  const player2Input = document.getElementById("form-input-2");
+  const winnerDisplay = document.querySelector(".winner-display");
 
   const board = GameBoard.getBoard();
 
   const p1 = {
     mark: player1.getMark(),
-    player: player1.getName(),
   };
   const p2 = {
     mark: player2.getMark(),
-    player: player2.getName(),
   };
   let active = p1;
 
   const switchActivePlayer = () => {
     active === p1 ? (active = p2) : (active = p1);
   };
+
+  const congratulateWinner = (winner) =>
+    (winnerDisplay.textContent = `Winner: ${winner}`);
 
   const checkForWinner = (active) => {
     let winner;
@@ -81,10 +88,10 @@ const GameController = (() => {
 
     winningCombinations.forEach((arr) => {
       if (arr.every((cell) => board[cell] === active.mark)) {
-        winner = active.player;
-        winner === "player1"
-          ? player1.congratulateWinner()
-          : player2.congratulateWinner();
+        winner = active;
+        active === p1
+          ? congratulateWinner(player1.getName())
+          : congratulateWinner(player2.getName());
         showNewGameBtn();
       }
     });
@@ -118,7 +125,43 @@ const GameController = (() => {
     newGameBtn.classList.add("hidden");
     gameBoard.classList.remove("fade");
     gameBoard.addEventListener("click", playGame);
+    winnerDisplay.textContent = "";
   };
+
+  const submitName = (player, input, playerName, form, display) => {
+    if (!input.value) return;
+    playerName.textContent = input.value;
+    player.changeName(input.value);
+    form.classList.add("hidden");
+    display.classList.remove("hidden");
+  };
+
+  const editName = (display, form) => {
+    display.classList.add("hidden");
+    form.classList.remove("hidden");
+  };
+
+  playerDisplay.addEventListener("click", (e) => {
+    if (e.target.id === "edit-icon-1") editName(player1Display, player1Form);
+    if (e.target.id === "edit-icon-2") editName(player2Display, player2Form);
+
+    if (e.target.id === "submit-btn-1")
+      submitName(
+        player1,
+        player1Input,
+        player1Name,
+        player1Form,
+        player1Display
+      );
+    if (e.target.id === "submit-btn-2")
+      submitName(
+        player2,
+        player2Input,
+        player2Name,
+        player2Form,
+        player2Display
+      );
+  });
 
   newGameBtn.addEventListener("click", resetGame);
 
